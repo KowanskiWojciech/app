023828e60e67bd307cce273d9ef0b6f07c2ac4be
Wojciech Kowański <kowanski.wojciech@gmail.com>
2020-05-12 21:19:31 +0200

    Updates

---
+++ precache-manifest.9b05d601c2df212dffffbc976e15c91e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e21303a2f4f98e2cbbf9ba48048df78",
+    "url": "/app/index.html"
+  },
+  {
+    "revision": "39cbcce0caf96d514ee3",
+    "url": "/app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "76fe8f9686565956a899",
+    "url": "/app/static/js/2.03ce0df4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/app/static/js/2.03ce0df4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39cbcce0caf96d514ee3",
+    "url": "/app/static/js/main.7f2349b2.chunk.js"
+  },
+  {
+    "revision": "dba14825de9bae28513d",
+    "url": "/app/static/js/runtime-main.9a6d5272.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/app/static/media/logo.5d5d9eef.svg"
+  }
+]);
